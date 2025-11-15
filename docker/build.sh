2af4d16542e1a00ce41a385583040cddebc23dfe
Minseok Choe <choems@outlook.com>
2025-11-15 15:57:25 +0900

    fix

--- docker/build.sh
+++ docker/build.sh
@@ -13,9 +13,9 @@ echo "installed JAVA_HOME=${JAVA_HOME}"
 
 cd /
 git clone https://github.com/Haiyang-Sun/nodeprof.js.git /works/nodeprof.js
+cd /works/nodeprof.js
 # pinned to 2025-11-15 for stablity, the commit it references is from 2023-09-12. Update as needed.
 git switch -c build-5cccefc 5cccefc56dbd144f6a9ff303288dc1f4441f30d4
-cd /works/nodeprof.js
 
 mx sforceimport
 
